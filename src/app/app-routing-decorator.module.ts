7d2fd8271cbd41e11eb9e71d47ff057ad684dc48
unknown <j.cserko@gmail.com>
2020-09-27 18:00:35 +0200

    Feat: documentation.

--- src/app/app-routing-decorator.module.ts
+++ src/app/app-routing-decorator.module.ts
@@ -1,3 +1,17 @@
+/**
+ * Annotated Router implementation for Angular applications.
+ * Usage:
+ * 1. Import module into the app.module and place it the imports array.
+ * 2. Set a Route in the corresponding component, with a Classdecorator:
+ * @Route({path: 'login'})
+ * export class LoginComponent { ...
+ * 3. You can also set a lazy loaded Route module from any component:
+ * @Route({
+ * path: 'admin',
+ * loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+ * })
+ * export class AppComponent { ...
+ */
 import { Component, NgModule, Type } from '@angular/core';
 import { RouterModule, Router, Route as R } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
